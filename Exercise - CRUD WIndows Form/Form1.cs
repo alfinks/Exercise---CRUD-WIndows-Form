0222e8af0bf07d062af64b60f836387fd4dec8a1
alfinks <maulanaafiansyah@gmail.com>
2022-06-17 14:02:16 +0700

    Add project files.

---
+++ Exercise - CRUD WIndows Form/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise___CRUD_WIndows_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'helmDataSet.Arai' table. You can move, or remove it, as needed.
+            this.araiTableAdapter.Fill(this.helmDataSet.Arai);
+
+        }
+    }
+}
